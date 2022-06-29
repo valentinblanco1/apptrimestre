6852dc8b92abdb5bb6359546730bd21c671f563a
valentinblanco1 <blanco.valentin.epet20@gmail.com>
2022-06-29 19:33:36 -0300

    login

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-const elemento = document.getElementById('h1');
-elemento.textContent = "Valentin Blanco Kombolitis";
+const express =  require('express');
+const app = express();
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
+
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+const bcryptjs = require('bcryptjs');
+
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized:true
+}));
+
+const connection = require('./database/db');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+
+app.listen(3000, (req, res)=>{
+    console.log('El Servidor esta Funcionando en http://localhost:3000');
+})
